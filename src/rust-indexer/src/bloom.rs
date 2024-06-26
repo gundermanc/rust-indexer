29b73c4693673441a4a6b38904a2d776fe53bb32
Christian Gunderman <gundermanc@gmail.com>
2024-06-25 21:18:48 -0700

    Bloom filter implementation.

---
+++ src/rust-indexer/src/bloom.rs
@@ -0,0 +1,69 @@
+use std::iter::Iterator;
+
+struct BloomFilter {
+    filter: u32
+}
+
+impl BloomFilter {
+    pub fn new(inputs: &[u32]) -> BloomFilter {
+
+        let mut filter = 0;
+
+        for input in inputs {
+            filter = filter | (input % u32::MAX);
+        }
+        
+        BloomFilter {
+            filter: filter
+        }
+    }
+
+    pub fn possibly_contains(&self, other: &BloomFilter) -> bool {
+        return (self.filter & other.filter) == other.filter;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::BloomFilter;
+
+    #[test]
+    fn bloom_empty() {
+        let filter = BloomFilter::new(&[]);
+
+        // Empty matches empty.
+        let empty_query = BloomFilter::new(&[]);
+        assert!(filter.possibly_contains(&empty_query));
+
+        // Empty does not match non-empty.
+        let non_empty_query = BloomFilter::new(&[10]);
+
+        assert!(!filter.possibly_contains(&non_empty_query));
+    }
+
+    #[test]
+    fn bloom_exact_match() {
+        let filter = BloomFilter::new(&[4, 1]);
+
+        let query = BloomFilter::new(&[1, 4]);
+        assert!(filter.possibly_contains(&query));
+    }
+
+    #[test]
+    fn bloom_subset() {
+        let filter = BloomFilter::new(&[0b0001, 0b0010, 0b0100]);
+
+        let query = BloomFilter::new(&[0b0100, 0b0101]);
+        assert!(filter.possibly_contains(&query));
+        assert!(!query.possibly_contains(&filter));
+    }
+
+    #[test]
+    fn bloom_same_bytes_different_combo() {
+        let filter = BloomFilter::new(&[0b0001, 0b0010, 0b0100]);
+
+        let query = BloomFilter::new(&[0b0101]);
+        assert!(filter.possibly_contains(&query));
+        assert!(!query.possibly_contains(&filter));
+    }
+}
---
+++ src/rust-indexer/src/trigram.rs
@@ -0,0 +1,78 @@
+
+#[derive(Debug)]
+struct Trigram {
+    first: u8,
+    second: u8,
+    third: u8
+}
+
+impl Trigram {
+    pub fn from_str(text: &str) -> Vec<Trigram> {
+        let text_bytes = text.as_bytes();
+        let trigrams_count = count_trigrams(text_bytes);
+
+        let mut trigrams = Vec::<Trigram>::with_capacity(trigrams_count);
+
+        for i in 0..trigrams_count {
+            let trigram = Trigram {
+                first: text_bytes[i],
+                second: text_bytes[i + 1],
+                third: text_bytes[i + 2],
+            };
+
+            trigrams.push(trigram);
+        }
+
+        trigrams
+    }
+}
+
+impl PartialEq<Trigram> for str {
+    fn eq(&self, other: &Trigram) -> bool {
+        let self_bytes = self.as_bytes();
+
+        self_bytes[0] == other.first &&
+            self_bytes[1] == other.second &&
+            self_bytes[2] == other.third
+    }
+}
+
+fn count_trigrams(bytes: &[u8]) -> usize {
+     ((bytes.len() as isize) - 2).max(0) as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::trigram::Trigram;
+
+    #[test]
+    fn trigram_empty() {
+        let trigram = Trigram::from_str("");
+        assert_eq!(0, trigram.len());
+    }
+
+    #[test]
+    fn trigram_less_than_one() {
+        let trigram = Trigram::from_str("a");
+        assert_eq!(0, trigram.len());
+
+        let trigram = Trigram::from_str("ab");
+        assert_eq!(0, trigram.len());
+    }
+
+    #[test]
+    fn trigram_one() {
+        let trigram = Trigram::from_str("abc");
+        assert_eq!(1, trigram.len());
+        assert_eq!("abc", trigram.get(0).unwrap());
+    }
+
+    #[test]
+    fn trigram_two() {
+        let trigram = Trigram::from_str("abcde");
+        assert_eq!(3, trigram.len());
+        assert_eq!(*"abc", trigram[0]);
+        assert_eq!(*"bcd", trigram[1]);
+        assert_eq!(*"cde", trigram[2]);
+    }
+}
